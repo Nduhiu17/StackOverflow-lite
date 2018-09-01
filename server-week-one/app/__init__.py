30d7cc4b7b0ac973fecf7017ae6e5eed5f8febbd
Your Name <nduhiu254@gmail.com>
2018-09-01 18:45:11 +0300

    Initialize post url for question resource

--- server-week-one/app/__init__.py
+++ server-week-one/app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_restplus import Api
 
-
+from app.resources import QuestionResource
 from config import DevelopmentConfig
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 api = Api(app)
+
+api.add_resource(QuestionResource, '/api/v1/questions','/api/v1/questions')
+
 
 
