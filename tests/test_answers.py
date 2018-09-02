79b1f60f05a8dd6b468ec5b5e8e324d8ac915494
Your Name <nduhiu254@gmail.com>
2018-09-02 15:37:13 +0300

    Add end point tests for posting an answer and resouce method to post an answer

--- tests/test_answers.py
+++ tests/test_answers.py
@@ -23,3 +23,16 @@ class TestAnswer(unittest.TestCase):
         self.new_answer = Answer(body="This is how to init python how to init python how to init python")
         self.assertTrue(type(self.new_answer.id), int)
         self.assertEqual(type(self.new_answer), Answer)
+
+    def test_question_posted(self):
+        # method to test an answer can be posted
+        new_answer = {'body': 'error sit voluptatem accusantium doloremque laudantiumerror sit volupta'}
+        response = self.client.post('/api/v1/question/anwsers', data=json.dumps(new_answer),headers={'Content-Type': 'application' '/json'})
+        self.assertEqual(response.status_code, 201)
+
+    def test_post_short_answer_body(self):
+        # test cant post with a short answer body
+        new_answer = {'body': 'short body'}
+        response = self.client.post('/api/v1/question/anwsers', data=json.dumps(new_answer),
+                                    headers={'Content-Type': 'application' '/json'})
+        self.assertEqual(response.status_code, 400)
