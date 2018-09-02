6d524150aa4f7175e9e794cb6bd4942a356b2571
Your Name <nduhiu254@gmail.com>
2018-09-02 14:57:51 +0300

    Add Answer class model and a method to save an answer

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from datetime import datetime
 #create a mock database
 MOCK_DATABASE = {
     "questions": [ ],
+    "answers":[ ],
 
 }
 
@@ -51,3 +52,28 @@ class Question:
             "date_modified": str(self.date_modified)
         }
         return obj
+
+    class Answer:
+        '''Class to model an answer'''
+
+        def __init__(self, body):
+            # method to initialize Answer class
+            self.id = uuid.uuid4()
+            self.body = body
+            self.date_created = datetime.now()
+            self.date_modified = datetime.now()
+
+        def save(self):
+            # method to save an answer
+            MOCK_DATABASE[ "answers" ].append(self)
+            return self.json_dumps()
+
+        def json_dumps(self):
+            # method to return a json object from the answer details
+            ans = {
+                "id": str(self.id),
+                "body": self.body,
+                "date_created": str(self.date_created),
+                "date_modified": str(self.date_modified)
+            }
+            return ans
